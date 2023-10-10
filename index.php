321f08c9e799646c771844fde71223bb52b547c6
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-10-10 15:08:07 +0200

    print in page all the hotels

--- index.php
+++ index.php
@@ -1,5 +1,46 @@
+<!-- Stampare tutti i nostri hotel con tutti i dati disponibili. -->
+
 <?php
 
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
 
 ?>
 
@@ -7,7 +48,7 @@
 <html lang="en">
 
 <head>
-    <title>Title</title>
+    <title>HOTELS</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -18,15 +59,28 @@
 </head>
 
 <body>
-    <header>
-        <!-- place navbar here -->
-    </header>
-    <main>
 
-    </main>
-    <footer>
-        <!-- place footer here -->
-    </footer>
+    <div class="container my-5">
+        <ul>
+
+            <?php foreach ($hotels as $key => $hotel) : ?>
+
+                <?php if ($hotel['parking'] === true) {
+                    $parking = 'parcheggio disponibile';
+                } else {
+                    $parking = 'parcheggio non disponibile';
+                }
+                ?>
+
+                <li><?php echo $hotel['name'] . ' - ' . $hotel['description'] . ' | ' . $parking . ' | ' . 'voto: ' . $hotel['vote'] . ' distanza dal centro ' . $hotel['distance_to_center'] . ' km'; ?></li>
+            <?php endforeach; ?>
+            <?php ?>
+
+
+        </ul>
+    </div>
+
+
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
     </script>
